acd498bb7f605b8f1a7fbb532688dec1e446369a
Cesare0356 <31817744+Cesare0356@users.noreply.github.com>
2024-06-10 18:26:23 +0200

    Update dataclient.h

--- header/dataclient.h
+++ header/dataclient.h
@@ -1,7 +1,9 @@
-#include <ctype.h>
 #include <pthread.h>
 #include <unistd.h>
 
 //Funzioni da implementare
+char to_up(char c);
+void to_uppstr(char *str);
+void handle_fine();
+void handle_sig(int sig);
 void client_fun(char* nome_server, int porta_server);
-void gestisci_client();
