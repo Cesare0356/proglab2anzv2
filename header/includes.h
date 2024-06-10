b9f7a3884596cf9d94279ef17f353590889acfa4
Cesare0356 <31817744+Cesare0356@users.noreply.github.com>
2024-06-10 18:26:51 +0200

    Update includes.h

--- header/includes.h
+++ header/includes.h
@@ -19,17 +19,18 @@
 #define MSG_PUNTI_FINALI 'F'
 #define MSG_PUNTI_PAROLA 'P'
 #define MSG_CHIUSURA_CLIENT 'C'
-#define MSG_CLASSIFICA 'Z'
-#define MATRIX_SIZE 16
+#define MSG_CLASSIFICA_PRONTA 'Z'
 #define SYSC(v, c, m) \
 if ((v = c) == -1) { perror(m); exit(errno); }
 #define SYSCN(v, c, m) \
 if ((v = c) == NULL) { perror(m); exit(errno); }
+//Struttura dati per inviare e ricevere messaggi
 typedef struct {
     char type;
     unsigned int length;
     char data[256];
 }message_info;
+
 //Funzioni da implementare per inviare messaggi client-server e viceversa
 void send_message(int socket, message_info* msg);
 void receive_message(int socket, message_info* msg);
